bda27b6e6528281528a10aaca666ec33ef1e659d
Tushar Garg <tushargarg50@gmail.com>
2022-05-08 16:59:46 +0530

    Contains Duplicate

---
+++ src/main/java/com/tg/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+package com.tg;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+ */
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if (!set.add(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
