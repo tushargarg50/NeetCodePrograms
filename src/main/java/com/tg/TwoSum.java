0993e472df18fbe73f72d9a790e14c30c727a3ed
Tushar Garg <tushargarg50@gmail.com>
2022-05-08 20:04:21 +0530

    Two sum

--- src/main/java/com/tg/TwoSum.java
+++ src/main/java/com/tg/TwoSum.java
@@ -26,8 +26,8 @@ public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> indexMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            if(indexMap.containsKey(target-nums[i])) {
-                return new int[]{indexMap.get(target-nums[i]), i};
+            if (indexMap.containsKey(target - nums[i])) {
+                return new int[]{indexMap.get(target - nums[i]), i};
             } else {
                 indexMap.put(nums[i], i);
             }
