c4f4ab5e14aff872ed85d35a6be9b4e9592a7cfb
Tushar Garg <tushargarg50@gmail.com>
2022-05-08 20:04:48 +0530

    Two sum

--- src/test/java/com/tg/TwoSumTest.java
+++ src/test/java/com/tg/TwoSumTest.java
@@ -2,26 +2,27 @@ package com.tg;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class TwoSumTest {
 
     @Test
     public void twoSumBruteForce() {
-        int[] numbers = new int[] {2,7,11,15};
+        int[] numbers = new int[]{2, 7, 11, 15};
         int target = 9;
         TwoSum twoSum = new TwoSum();
         int[] result = twoSum.twoSumBruteForce(numbers, target);
         assertNotNull(result);
         assertEquals(0, result[0]);
         assertEquals(1, result[1]);
-        numbers = new int[]{3,2,4};
+        numbers = new int[]{3, 2, 4};
         target = 6;
         result = twoSum.twoSumBruteForce(numbers, target);
         assertNotNull(result);
         assertEquals(1, result[0]);
         assertEquals(2, result[1]);
-        numbers = new int[]{3,3};
+        numbers = new int[]{3, 3};
         result = twoSum.twoSumBruteForce(numbers, target);
         assertNotNull(result);
         assertEquals(0, result[0]);
@@ -31,20 +32,20 @@ public class TwoSumTest {
 
     @Test
     public void twoSum() {
-        int[] numbers = new int[] {2,7,11,15};
+        int[] numbers = new int[]{2, 7, 11, 15};
         int target = 9;
         TwoSum twoSum = new TwoSum();
         int[] result = twoSum.twoSum(numbers, target);
         assertNotNull(result);
         assertEquals(0, result[0]);
         assertEquals(1, result[1]);
-        numbers = new int[]{3,2,4};
+        numbers = new int[]{3, 2, 4};
         target = 6;
         result = twoSum.twoSum(numbers, target);
         assertNotNull(result);
         assertEquals(1, result[0]);
         assertEquals(2, result[1]);
-        numbers = new int[]{3,3};
+        numbers = new int[]{3, 3};
         result = twoSum.twoSum(numbers, target);
         assertNotNull(result);
         assertEquals(0, result[0]);
